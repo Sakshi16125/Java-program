6d398eaf1318fad3901d15a73e5bb79698f6046a
Sakshi singh <sakshi.singh.16125@gmail.com>
2026-01-15 20:27:21 +0530

    Add Reverse Star Pattern Java program

---
+++ Reverse_Star_pattern.java
@@ -0,0 +1,14 @@
+// class name
+public class Reverse_Star_pattern {
+	public static void main(String[] args) {
+		
+		for(int i = 5; i >= 1; i--){    // value of outer loop
+			for(int j = 1; j <= i; j++){   // value of inner loop
+				System.out.print("*");  // print star
+					}
+				System.out.println();
+		           } 
+	           }
+            }
+          
+		     
